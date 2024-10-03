3339f5908773da669bfd8b01c4423b50bc7e9105
KArtem <k6art5@gmail.com>
2024-10-03 14:52:34 +0700

    wip

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,14 +1,5 @@
-import { useState } from 'react'
-import WebApp from '@twa-dev/sdk'
-import reactLogo from '../../shared/assets/react.svg'
-import viteLogo from '/vite.svg'
 import { NavLink } from 'react-router-dom'
-import clns from 'classnames/bind'
 import { Button } from '../../shared/ui'
-
-import styles from './Main.module.scss'
-
-const cx = clns.bind(styles);
 
 export const Main = () => {
     return (
