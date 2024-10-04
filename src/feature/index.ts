65d237250d9f2b4e8cc0310032f1fb3b90e7eba4
KArtem <k6art5@gmail.com>
2024-10-04 14:14:46 +0700

    create import feature

---
+++ src/feature/index.ts
@@ -0,0 +1,4 @@
+export { ButtonChangeMode } from './ButtonChangeMode'
+export { ButtonWallet } from './ButtonWallet'
+export { ButtonBurger } from './ButtonBurger'
+export { ButtonBet } from './ButtonBet'
