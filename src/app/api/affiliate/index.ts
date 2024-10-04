3ef3d39b437b839de8e0884af0e2ae168f6f2abb
KArtem <k6art5@gmail.com>
2024-10-04 18:46:13 +0700

    added api

---
+++ src/app/api/affiliate/index.ts
@@ -0,0 +1,15 @@
+import { get, post, deleteMethod} from '../core';
+
+const url = `${process.env.REACT_APP_API_URL_AFFILIATE}`;
+
+export const signIn = (data = {}) => post(`${url}/signin`, data);
+export const signUp = (data = {}) => post(`${url}/signup`, data);
+export const requestPayout = (data = {}) => post(`${url}/api/request_payout`, data);
+
+export const getEarnings = () => get(`${url}/api/get_earnings`);
+export const viewProfile = () => get(`${url}/api/view_profile`);
+export const createInviteLink = (data = {}) => post(`${url}/api/create_invite_link`, data);
+export const deleteInviteLink = (data = {}) => deleteMethod(`${url}/api/delete_invite_link`, data);
+
+// TODO: change from papi to api - some papi for test
+export const addLinkInvitee = (data = {}) => post(`${url}/papi/add_link_invitee`, data);
---
+++ src/app/api/core.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+import { getStorage } from '../../utils/localStorage';
+
+interface Headers {
+  'Content-Type': string,
+  'Authorization'?: string
+}
+
+const headers: Headers = {
+  'Content-Type': 'application/json',
+}
+
+const getHeaders = () => {
+  const token = getStorage('token');
+
+  if (token) {
+    headers['Authorization'] = `Bearer ${token}`;
+  }
+
+  return headers;
+}
+
+export const get = async (url: string) => (
+  axios.get(url,{ headers: { ...getHeaders() }})
+);
+
+export const post = async (url: string, data = {}) => (
+  axios.post(url, data, { headers: { ...getHeaders()}})
+);
+
+export const deleteMethod = async (url: string, data = {}) => (
+  axios.delete(url, {data, headers: { ...getHeaders()}})
+);
