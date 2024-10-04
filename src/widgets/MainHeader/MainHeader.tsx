48298f4185589938fa0ed402953c24b9d0af166b
KArtem <k6art5@gmail.com>
2024-10-04 14:19:30 +0700

    create MainHeader widget

---
+++ src/widgets/MainHeader/MainHeader.tsx
@@ -0,0 +1,53 @@
+import classNames from 'classnames/bind'
+import styles from './MainHeader.module.scss'
+
+import { useModal } from '../../hooks'
+
+import {
+	ButtonChangeMode,
+	ButtonWallet,
+	ButtonBurger
+} from '../../feature/'
+
+const cx = classNames.bind(styles)
+
+export const MainHeader = () => {
+	const {
+		// isOpen: isOpenWallet,
+		openModalHandler: openWallet,
+		closeModalHandler: closeWallet,
+	  } = useModal();
+
+	const {
+		// isOpen: isOpenModalAuth,
+		openModalHandler: openModalAuth,
+		closeModalHandler: closeModalAuth,
+		} = useModal();
+
+	const handlerChangeMode = () => {
+		console.log('change mode')
+	}
+	const handlerModalAuth = () => {
+		openModalAuth()
+		console.log('show modal');
+		
+		setTimeout(closeModalAuth, 1000);
+	}
+
+	const handlerWallet = () => {
+		openWallet()
+		console.log('show wallet');
+
+		setTimeout(closeWallet, 1000);
+	}
+
+	return (
+		<header className={cx('header')}>
+			<div className={cx('header__button')}>
+				<ButtonChangeMode onClick={handlerChangeMode} />
+				<ButtonWallet onClick={handlerWallet} />
+			</div>
+			<ButtonBurger onClick={handlerModalAuth} />
+		</header>
+	)
+}
