7df74d57e10aba4a0d9fafd151d12ba3737081cf
KArtem <k6art5@gmail.com>
2024-10-06 18:22:37 +0700

    edited showTooltip last price

--- src/widgets/Chart/utils/showTooltip.ts
+++ src/widgets/Chart/utils/showTooltip.ts
@@ -11,50 +11,22 @@ export const showTooltip: Plugin<"line"> = {
         ? chart.getDatasetMeta(0).data[89]
         : { x: 0, y: 0 };
 
-    const widthTooltip = 110;
-    const heightTooltip = 30;
-    const marginX = x + 10;
+    const widthTooltip = 83;
+    const heightTooltip = 24;
+    const marginX = x + 5;
 
-    ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
+    ctx.fillStyle = "#FFFFFF14";
     ctx.beginPath();
-    ctx.roundRect(marginX, y - 10, widthTooltip, heightTooltip, [8]);
+    ctx.roundRect(marginX, y - 8, widthTooltip, heightTooltip, 8);
     ctx.fill();
     ctx.restore();
 
-    ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
-    ctx.beginPath();
-    ctx.moveTo(marginX + 15, y - 10);
-    ctx.lineTo(marginX + widthTooltip - 15, y - 10);
-    ctx.lineTo(marginX + widthTooltip - 25, y - 20);
-    ctx.lineTo(marginX + 25, y - 20);
-    ctx.closePath();
-    ctx.fill();
-
-    ctx.restore();
-
     // text
-    const gradient = ctx.createLinearGradient(0, y - 15, 0, y);
-    gradient.addColorStop(0, "rgb(251, 226, 70)");
-    gradient.addColorStop(1, "rgb(225, 131, 17)");
-    ctx.fillStyle = gradient;
-    ctx.font = "8px Unbounded";
-    ctx.textAlign = "center";
-    ctx.fillText("LIVE PRICE", marginX + widthTooltip / 2, y - 8);
-    ctx.restore();
-
-    // text
-    ctx.font = "12px Unbounded";
-    if (options.startPrice !== 0) {
-      ctx.fillStyle =
-        options.label > options.startPrice
-          ? "rgb(145, 255, 93)"
-          : "rgb(255, 50, 50)";
-    } else {
-      ctx.fillStyle = "white";
-    }
+    ctx.font = "14px Inter";
+    ctx.fillStyle = "white";
     ctx.textAlign = "center";
     ctx.fillText(
-      "$ " + new Intl.NumberFormat("en").format(options.label),
+      Number(options.label).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&.'),
       marginX + widthTooltip / 2,
       y + 10
     );
