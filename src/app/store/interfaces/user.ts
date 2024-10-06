b06d1cf61ac31b53ab3f8034d882c1978f551861
KArtem <k6art5@gmail.com>
2024-10-06 23:31:06 +0700

    correct store

---
+++ src/app/store/interfaces/user.ts
@@ -0,0 +1,9 @@
+export interface IUser {
+    chain: string,
+    publicKey: string,
+    address: string,
+    appName: string,
+    appVersion: string,
+    maxProtocolVersion: string,
+    platform: string,
+}
---
+++ src/app/store/slices/userSlice.ts
@@ -0,0 +1,41 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { IUser } from '../interfaces/user';
+
+const initialState: IUser = {
+    chain: '',
+    publicKey: '',
+    address: '',
+    appName: '',
+    appVersion: '',
+    maxProtocolVersion: '',
+    platform: '',
+}
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        setUser(state, action) {
+            state.chain = action.payload.chain;
+            state.publicKey = action.payload.publicKey;
+            state.address = action.payload.address;
+            state.appName = action.payload.appName;
+            state.appVersion = action.payload.appVersion;
+            state.maxProtocolVersion = action.payload.maxProtocolVersion;
+            state.platform = action.payload.platform;
+        },
+        removeUser(state) {
+            state.chain = initialState.chain
+            state.publicKey = initialState.publicKey
+            state.address = initialState.address
+            state.appName = initialState.appName
+            state.appVersion = initialState.appVersion
+            state.maxProtocolVersion = initialState.maxProtocolVersion
+            state.platform = initialState.platform
+        }
+    }
+});
+
+export const { setUser, removeUser } = userSlice.actions;
+
+export default userSlice.reducer;
