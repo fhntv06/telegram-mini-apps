f23e3f90b22b579fefb7694acb6738296a1fe73e
kartem <kartemake@gmail.com>
2024-10-07 21:17:19 +0700

    added useContext Game Status Context in ChartPanel

--- src/widgets/ChartPanel/ChartPanel.tsx
+++ src/widgets/ChartPanel/ChartPanel.tsx
@@ -1,25 +1,31 @@
+import { useContext } from 'react'
 import classNames from 'classnames/bind'
+import { GameStatusContext } from '../../app/contexts'
+import { differenceInSeconds } from '../../shared/utils'
+
 import styles from './ChartPanel.module.scss'
 
 const cx = classNames.bind(styles)
 
 export const ChartPanel = () => {
-  const upProcent = 123
-  const downProcent = 132
+  const {
+    phaseTimeUntil,
+    winPercent: { downPercent, upPercent }
+  } = useContext(GameStatusContext);
 
   return (
     <div className={cx('panel')}>
       <div className={cx('panel__bet-type')}>
         <h2>UP WINS</h2>
-        <p className={cx('panel__bet-procent', 'up')}>{upProcent}%</p>
+        <p className={cx('panel__bet-percent', 'up')}>{upPercent}%</p>
       </div>
       <div className={cx('panel__bet-type', 'time')}>
         <h2>GAME IN PROCESS</h2>
-        <h1>00:16</h1>
+        <h1>{differenceInSeconds(phaseTimeUntil)}</h1>
       </div>
       <div className={cx('panel__bet-type', 'down')}>
         <h2>DOWN WINS</h2>
-        <p className={cx('panel__bet-procent', 'down')}>{downProcent}%</p>
+        <p className={cx('panel__bet-percent', 'down')}>{downPercent}%</p>
       </div>
     </div>
   )
