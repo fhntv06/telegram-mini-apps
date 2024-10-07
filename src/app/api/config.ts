15952c2807d35cd765ed55364113aed00a96a21e
kartem <kartemake@gmail.com>
2024-10-07 21:12:20 +0700

    fix url api

--- src/app/api/config.ts
+++ src/app/api/config.ts
@@ -1,2 +1,1 @@
-// export const url = `${process.env.REACT_APP_API_URL_GAME}`;
-export const url = `http://nemitor.ru:3000/swagger/`;
+export const url = `${import.meta.env.REACT_APP_API_URL_GAME}`;
