5b02975ae81620d1c4f9cfbc1892e0fc523f6730
KArtem <k6art5@gmail.com>
2024-10-07 08:38:44 +0700

    change initial data

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,58 +1,30 @@
-import { EStatusText, IGameStatus } from './providers/types';
+import { IGameStatus } from './providers/types';
 
 export const initialData: IGameStatus = {
-  PoolInfo: {
-    Created: true,
-    StartPrice: 0,
-    EndPrice: 0,
-    MinBetAmount: 0,
-    MaxBetAmount: 1000000000000000000000,
-    PoolBetsLimit: 100,
-    UpBetGroup: {
-      Bets: [],
-      Addresses: [],
-      Avatars: [],
-      Countries: [],
-      WhiteLabelIds: [],
-      Total: 0,
-      DistributedCount: 0,
-      TotalDistributed: 0,
-    },
-    DownBetGroup: {
-      Bets: [],
-      Addresses: [],
-      Avatars: [],
-      Countries: [],
-      WhiteLabelIds: [],
-      Total: 0,
-      DistributedCount: 0,
-      TotalDistributed: 0,
-    },
-    RoundStartTime: 1712964171000000,
-    TradesStartTimeMS: 1712964186,
-    TradesEndTimeMS: 1713568986,
-  },
-  PriceInfo: {
-    lastPrice: 0,
-    lastTime: '',
-    lastTimeMs: 0
-  },
-  InternalInfo: {
-    EnableTrade: true,
-    Time2End: 0,
-    Text: EStatusText.Open,
-  },
-  JackpotInfo: {
-    JackpotBalance: 0,
-  },
-  NewResult: {
-    StartPrice: 0,
-    EndPrice: 0,
-    Wins: null,
-    Hash: "",
-    Timestamp: 0,
-  },
-  LastResults: {
-    Data: [],
-  },
+  upPoolData: {
+		playersImg: [],
+		betPool: 0
+	},
+	downPoolData: {
+		playersImg: [],
+		betPool: 0
+	},
+	totalBets : 0,
+	btcPrice: 0,
+	startBtcPrice: 0,
+	gamePhase: 0,
+	phaseTimeUntil: 0,
+	gameResult: 0,
+	/*
+	 *  0 - down
+	 *  1 - up
+	 *  2 - refund
+	 */
+	last3GamesRes: [],
+	livePlayers: 0,
+	allTimeWins: '',
+	winPercent: {
+		downPercent : 0,
+		upPercent : 0,
+	}
 };
