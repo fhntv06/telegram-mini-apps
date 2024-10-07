7bf7f746f4248931e20a7502da14043a18c43096
kartem <kartemake@gmail.com>
2024-10-07 21:10:56 +0700

    create panel result in BetPanel

--- src/widgets/BetPanel/BetPanel.tsx
+++ src/widgets/BetPanel/BetPanel.tsx
@@ -1,9 +1,10 @@
+import { useId } from 'react'
 import classNames from 'classnames/bind'
 import styles from './BetPanel.module.scss'
 import { ButtonPlaceBet } from '../../feature'
 import { IDataPanel } from './types'
-import { Icon, Person } from '../../shared'
-import { formatIntTonNumber } from '../../shared/utils'
+import {Icon, Person } from '../../shared'
+import {formatIntTonNumber, formatNumber} from '../../shared/utils'
 const cx = classNames.bind(styles)
 
 interface Props {
@@ -16,49 +17,50 @@ const titleResultPanel = {
 	down: 'DOWN WINNER'
 }
 
+const IsPerson = (img: string) => <Person key={useId()} img={img} />
+
 export const BetPanel = ({ data, type = 'up' }: Props) => {
 	const handlerBet = () => {
 		console.log('Bet is ', type)
 	}
 
-	const completedRound = true
-
-	const { personsImg, betPool } = data;
-
-	const count = personsImg.length - 5
+	const completedRound = false
+	const { playersImg, betPool } = data;
+	const count = playersImg.length - 5
 
 	return (
-		<div className={cx('panel')}>
-			{
-				completedRound ? (
-					<div className={cx('panel__result')}>
-							<p className={cx('panel__result__title')}>{titleResultPanel[type]}</p>
-							<p className={cx('panel__result__text')}>
-								<Icon name='ton' />
-								{betPool}
-							</p>
-					</div>
-				) : (
-					<div className={cx('panel__data')}>
+		<div className={cx('panel', {'panel__result': completedRound, [type]: completedRound})}>
+			<div className={cx('panel__data')}>
+				{
+					completedRound ? (
+						<>
+							<p>{titleResultPanel[type]}</p>
+							<h1 className={cx('panel__result__text')}>
+								<Icon name='ton' size='big'/>
+								{formatNumber(betPool)}
+							</h1>
+						</>
+					) : (
+						<>
 						<div className={cx('panel__data__players')}>
 							<p className={cx('players__total', 'p p-small')}>
-								<Icon name='persons' />
-								{personsImg.length}
+								<Icon name='persons'/>
+								{playersImg.length}
 							</p>
 							<p className={cx('players__total', 'p p-small', type)}>
-								<Icon name='ton' />
+								<Icon name='ton'/>
 								{formatIntTonNumber(betPool)}
 							</p>
 						</div>
 						<div className={cx('panel__data__persons')}>
-							{personsImg.slice(0, 5).map((img) => (<Person key={img} img={img} />))}
+							{playersImg.slice(0, 5).map((img: string) => IsPerson(img))}
 							{count > 0 && <p className={cx('panel__persons__count')}><span>{`+${count}`}</span></p>}
 						</div>
-					</div>
+					</>
 				)
 			}
-
-			<ButtonPlaceBet type={type} onClick={handlerBet} />
+			</div>
+			{!completedRound && <ButtonPlaceBet type={type} onClick={handlerBet}/>}
 		</div>
 	)
 }
