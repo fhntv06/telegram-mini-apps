d76a0323ab3be2c499c393bf34f2ad648e82ffab
KArtem <k6art5@gmail.com>
2024-10-07 08:37:59 +0700

    change type initial in price history context

--- src/app/contexts/PriceHistoryContext.tsx
+++ src/app/contexts/PriceHistoryContext.tsx
@@ -1,5 +1,3 @@
 import { createContext } from 'react'
-import { initialData } from '../constants'
-import { IPriceInfo } from '../providers/types';
 
-export const PriceHistoryContext = createContext<IPriceInfo[]>([initialData.PriceInfo]);
+export const PriceHistoryContext = createContext<number[]>([]);
