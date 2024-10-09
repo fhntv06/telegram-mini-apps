d2efd0689005509c31c1751f405385de644d0b1d
kartem <kartemake@gmail.com>
2024-10-09 21:10:16 +0700

    refactor Main for new hooks

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,33 +1,33 @@
-import { Provider } from 'react-redux';
-import store from '../../app/store';
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
 import classNames from 'classnames/bind'
 
-import { MainHeader,
-  MainFooter,
-  // Chart,
-} from '../../widgets'
+import { MainHeader, MainFooter, Chart } from '../../widgets'
 
-import {
-  GameSocketProvider,
-  ModalProvider
-} from '../../app/providers'
+import { ModalProvider } from '../../app/providers'
 
 import styles from './Main.module.scss'
+import { useGameSocket, usePriceHistory } from '../../hooks/'
+import { setGameStatus } from '../../app/store/slices/game'
 
 const cx = classNames.bind(styles)
 
 export const Main = () => {
+  const dispatch = useDispatch();
+  const data = useGameSocket()
+  const priceHistory = usePriceHistory()
+
+  useEffect(() => {
+    dispatch(setGameStatus({ ...data, priceHistory}))
+  }, [data]);
+
   return (
-    <Provider store={store}>
-      <GameSocketProvider>
-        <ModalProvider>
-          <main className={cx('main')}>
-            <MainHeader />
-            {/*<Chart />*/}
-            <MainFooter />
-          </main>
-        </ModalProvider>
-     </GameSocketProvider>
-    </Provider>
+    <ModalProvider>
+      <main className={cx('main')}>
+        <MainHeader />
+        <Chart />
+        <MainFooter />
+      </main>
+    </ModalProvider>
   )
 }
