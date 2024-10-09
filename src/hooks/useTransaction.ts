ef5db23cfbd1a73c90d23dd0bc5f6a1a94a7f7e6
kartem <kartemake@gmail.com>
2024-10-09 19:22:46 +0700

    refactor hook useTransaction

--- src/hooks/useTransaction.ts
+++ src/hooks/useTransaction.ts
@@ -1,20 +1,23 @@
-import {ActionConfiguration, SendTransactionRequest, useTonConnectUI, useTonWallet} from '@tonconnect/ui-react'
-import {useState} from 'react'
-// import {getAddressContract} from '../app/api/game'
+import { ActionConfiguration, SendTransactionRequest, useTonConnectUI, useTonWallet } from '@tonconnect/ui-react'
+import { useState } from 'react'
+import { getAddressContract } from '../app/api/game'
 
 export const useTransaction = (amount: number) => {
   const wallet = useTonWallet()
   const [tonConnectUI] = useTonConnectUI()
   const [txInProcess, setTxInProcess] = useState<boolean>(false)
 
-  const sendTransaction = async () => {
+  const sendTransaction = async (placeBet: string = 'up') => {
     console.log('SendTransaction run!')
     alert('SendTransaction run!')
 
-    // const address = await getAddressContract()
+    const address = await getAddressContract()
+      .then((res) => res.data.address)
+      .catch((error) => new Error(error))
 
-    alert(`SendTransaction wallet is ${JSON.stringify(wallet, null, 2)}`)
-    // alert(`SendTransaction wallet is ${JSON.stringify(address, null, 2)}`)
+    alert(`SendTransaction address ${address}`)
+    alert(`SendTransaction amount ${amount}`)
+    alert(`SendTransaction bet place ${placeBet === 'up' ? import.meta.env.VITE_UP_TRANSACTION : import.meta.env.VITE_DOWN_TRANSACTION}`)
 
     if (!wallet) tonConnectUI.connectWallet()
     else {
@@ -29,8 +32,9 @@ export const useTransaction = (amount: number) => {
         validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec
         messages: [
           {
-            address: import.meta.env.VITE_ADDRESS_TRANSACTION,
+            address,// import.meta.env.VITE_ADDRESS_TRANSACTION,
             amount: (amount * 1e9).toString(), // Toncoin in nanotons
+            payload: placeBet === 'up' ? import.meta.env.VITE_UP_TRANSACTION : import.meta.env.VITE_DOWN_TRANSACTION
           },
         ],
       };
