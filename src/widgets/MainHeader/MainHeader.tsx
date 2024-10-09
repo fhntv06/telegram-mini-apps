376e1cd1cc9790fc0957f1e52d08f701f778e927
kartem <kartemake@gmail.com>
2024-10-09 19:33:05 +0700

    refactor in MainHeader

--- src/widgets/MainHeader/MainHeader.tsx
+++ src/widgets/MainHeader/MainHeader.tsx
@@ -1,30 +1,26 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { useSelector } from 'react-redux'
-import { ModalContext } from '../../app/contexts';
-import { ModalContextTypes } from '../../app/providers/types'
-
 import classNames from 'classnames/bind'
+
 import styles from './MainHeader.module.scss'
 
-import {
-	ButtonChangeMode,
-	ButtonWallet,
-	ButtonBurger
-} from '../../feature/'
+import { ModalContext } from '../../app/contexts'
+import { ModalContextTypes } from '../../app/providers/types'
+import { ButtonChangeMode, ButtonWallet, ButtonBurger } from '../../feature/'
 
 const cx = classNames.bind(styles)
 
 export const MainHeader = () => {
-	const { wallet } = useSelector((state: any) => state.user);
+	const { wallet, balance } = useSelector((state: any) => state.user)
 	const { openModalHandler } = useContext<ModalContextTypes>(ModalContext)
 
 	return (
 		<header className={cx('header')}>
 			<div className={cx('header__button')}>
-				<ButtonChangeMode text='$BTC, 30s' onClick={openModalHandler} />
-				{wallet && <ButtonWallet onClick={openModalHandler} />}
+				<ButtonChangeMode text='$BTC, 30s' onClick={() => openModalHandler('select__mode')} />
+				{wallet && <ButtonWallet balance={balance} onClick={() => openModalHandler('wallet')} />}
 			</div>
-			<ButtonBurger onClick={openModalHandler} />
+			<ButtonBurger onClick={() => openModalHandler('burger')} />
 		</header>
 	)
 }
