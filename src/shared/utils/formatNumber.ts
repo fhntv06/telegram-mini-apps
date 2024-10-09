7cbf2ee4c9a9023432aaa13e4856618965d46bd1
kartem <kartemake@gmail.com>
2024-10-09 19:24:33 +0700

    refactor func formatNumber

--- src/shared/utils/formatNumber.ts
+++ src/shared/utils/formatNumber.ts
@@ -1,3 +1,11 @@
-export const formatNumber = (number: number | string) => number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")
+export const formatNumber = (number: number | string) => {
+  let res = '0';
+
+  if (number) {
+    res = number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")
+  } else new Error(`formatNumber func , number is ${number}, ${typeof number}`);
+
+  return res
+}
 export const formatIntTonNumber = (number: number) => number / 1000000000
 export const convertNumberInTon = (number: number) => formatNumber(formatIntTonNumber(number))
