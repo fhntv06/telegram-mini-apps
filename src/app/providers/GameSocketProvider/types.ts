16da7bbb1abb49413fafaa6703e13dffd287d866
kartem <kartemake@gmail.com>
2024-10-10 21:44:40 +0700

    delete not needed providers

--- src/app/providers/GameSocketProvider/types.ts
+++
@@ -1,3 +0,0 @@
-export interface IPriseHistory {
-	lastPrice: number,
-}
--- src/app/providers/GameStatusProvider/index.tsx
+++
@@ -1,9 +0,0 @@
-import { GameStatusContext } from '../../contexts'
-
-export const GameStatusProvider = ({ data, children }: { data: any, children: React.ReactNode }) => {
-  return (
-    <GameStatusContext.Provider value={data}>
-      {children}
-    </GameStatusContext.Provider>
-  )
-}
