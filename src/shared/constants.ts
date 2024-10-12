2f41e777ded41c1e5dbd3a20ed6ade29ef013e50
KArtem <k6art5@gmail.com>
2024-10-12 14:02:27 +0700

    forming correct initial data in constants

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -11,25 +11,39 @@ export const initialDataPriceHistory: number[] = ((min = 62000, max = 62001) => 
 })();
 export const initialDataGameStatus: IGameStatus = {
   upPoolData: {
-		playersImg: [],
-		betPool: 0
+		playersImg: [
+			`/images/avatars/user_${Math.floor(Math.random() * (5 - 1 + 1) + 1)}.png`,
+			`/images/avatars/user_${Math.floor(Math.random() * (5 - 1 + 1) + 1)}.png`,
+			`/images/avatars/user_${Math.floor(Math.random() * (5 - 1 + 1) + 1)}.png`,
+			`/images/avatars/user_${Math.floor(Math.random() * (5 - 1 + 1) + 1)}.png`,
+		],
+		betPool: 25 * 10**9
 	},
 	downPoolData: {
-		playersImg: [],
-		betPool: 0
+		playersImg: [
+			`/images/avatars/user_${Math.floor(Math.random() * (5 - 1 + 1) + 1)}.png`,
+			`/images/avatars/user_${Math.floor(Math.random() * (5 - 1 + 1) + 1)}.png`,
+			`/images/avatars/user_${Math.floor(Math.random() * (5 - 1 + 1) + 1)}.png`,
+			`/images/avatars/user_${Math.floor(Math.random() * (5 - 1 + 1) + 1)}.png`,
+			`/images/avatars/user_${Math.floor(Math.random() * (5 - 1 + 1) + 1)}.png`,
+			`/images/avatars/user_${Math.floor(Math.random() * (5 - 1 + 1) + 1)}.png`,
+			`/images/avatars/user_${Math.floor(Math.random() * (5 - 1 + 1) + 1)}.png`,
+			`/images/avatars/user_${Math.floor(Math.random() * (5 - 1 + 1) + 1)}.png`,
+		],
+		betPool: 15 * 10**9
 	},
-	totalBets : 0,
-	btcPrice: 0,
-	startBtcPrice: 0,
+	totalBets: 50 * 10**9,
+	btcPrice: (Math.floor(Math.random() * (62001 - 62000 + 1))) + 62000,
+	startBtcPrice: (Math.floor(Math.random() * (62001 - 62000 + 1))) + 62000,
 	gamePhase: 0,
-	phaseTimeUntil: 0,
+	phaseTimeUntil: Date.now() + 30 * 1000,
 	gameResult: 0,
-	last3GamesRes: [],
-	livePlayers: 0,
-	allTimeWins: 0,
+	last3GamesRes: [Math.floor(Math.random() * (2 - 1 + 1)) + 1, Math.floor(Math.random() * (2 - 1 + 1)) + 1, Math.floor(Math.random() * (2 - 1 + 1)) + 1],
+	livePlayers: Math.floor(Math.random() * (2000 - 1 + 1)) + 1,
+	allTimeWins: Math.floor(Math.random() * (1000000 * 10**9 - 1 * 10**9 + 1 * 10**9)) + 1 * 10**9,
 	winPercent: {
-		downPercent : 0,
-		upPercent : 0
+		downPercent : 200,
+		upPercent : 200
 	},
 	// priceHistory: initialDataPriceHistory
 }
