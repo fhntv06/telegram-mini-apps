9e0de87f21d69af1713f6c19a2f6310eb6c87bb3
kartem <kartemake@gmail.com>
2024-10-14 18:13:55 +0700

    refactor reconnect game socket

--- src/hooks/useGameSocket.tsx
+++ src/hooks/useGameSocket.tsx
@@ -1,32 +1,24 @@
 import { useState, useEffect } from 'react'
 import { IGameStatus } from '../app/providers/types'
-import { initialDataGameStatus } from '../shared/constants'
+// import { initialDataGameStatus } from '../shared/constants'
 
 const urlSocket = `${import.meta.env.VITE_SOCKET_PROTOCOL}://${import.meta.env.VITE_DOMAIN}:${import.meta.env.VITE_PORT}`
 
 export const useGameSocket = () => {
-  const [data, setData] = useState<IGameStatus>();
-  const [error, setError] = useState<boolean>(false);
+  const [data, setData] = useState<IGameStatus>()
+  const [error, setError] = useState<boolean>(false)
 
-  useEffect(() => {
+  const handlerConnection = () => {
     const gameSocket = new WebSocket(urlSocket)
 
-    gameSocket.onopen = () => {
-      console.log("game socket connected");
-    };
+    gameSocket.onopen = () => console.log("game socket connected")
     gameSocket.onclose = (event) => {
-      if (event.wasClean) {
-        console.log("Соединение закрыто чисто");
-      } else {
-        console.log("Обрыв соединения"); // например, "убит" процесс сервера
-      }
-
+      console.log(event.wasClean ? "Соединение закрыто чисто" : "Обрыв соединения");
       console.log("Код: " + event.code + " причина: " + event.reason)
+      console.log('Переподключение к сокету!')
 
-      setError(true)
-      setData(initialDataGameStatus)
-      console.log('Подключение тестовых данных за место сокета! Проверьте соединение с сервером!')
-    };
+      setError((prev) => !prev)
+    }
     gameSocket.onmessage = (event) => {
       const {
         up_pool_data: { players_img: players_img_up, betPool: betPoolUp },
@@ -61,28 +53,27 @@ export const useGameSocket = () => {
 
       setData(data)
     };
-    gameSocket.onerror = (event) => {
-      console.log("game socket error: ", event)
-    }
-  }, [])
+    gameSocket.onerror = (event) => console.log("game socket error: ", event)
+  }
 
-  useEffect(() => {
-    if (error) {
-      const interval = setInterval(() => {
-        // Эмуляция получения нового сообщения каждую секунду
-        // Обновляем состояние с новыми данными
+  useEffect(handlerConnection, [error])
 
-        // @ts-ignore
-        setData((prevData: IGameStatus) => ({
-          ...prevData, // Сохраняем все предыдущие данные
-          btcPrice: (Math.random() * (62001 - 62000 + 1)) + 62000 // Обновляем поле priceHistory
-        }));
-
-      }, 500)
-
-      return () => clearInterval(interval)
-    }
-  }, [error]);
+  // test
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     // Эмуляция получения нового сообщения каждую секунду
+  //     // Обновляем состояние с новыми данными
+  //
+  //     // @ts-ignore
+  //     setData((prevData: IGameStatus) => ({
+  //       ...prevData, // Сохраняем все предыдущие данные
+  //       btcPrice: (Math.random() * (62001 - 62000 + 1)) + 62000 // Обновляем поле priceHistory
+  //     }));
+  //
+  //   }, 500)
+  //
+  //   return () => clearInterval(interval)
+  // }, [error]);
 
   return data
 }
