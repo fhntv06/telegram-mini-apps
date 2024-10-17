f282c5a09676da7b5369f40f7a4d304f102eff0c
KArtem <k6art5@gmail.com>
2024-10-17 18:23:21 +0700

    create useUserData

---
+++ src/hooks/useUserData.ts
@@ -0,0 +1,19 @@
+import { useEffect } from 'react'
+import WebApp from '@twa-dev/sdk';
+import { setUserDataTelegram } from '../app/store/slices/user'
+import { useDispatch } from 'react-redux';
+
+export const useUserData = () => {
+  const initData = WebApp.initDataUnsafe;
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (initData && initData.user) {
+      dispatch(
+        setUserDataTelegram(initData.user)
+      )
+    }
+  }, [initData])
+
+  return initData.user
+}
