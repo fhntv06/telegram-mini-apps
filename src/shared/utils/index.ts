8ec1c0c1a8bdba0e8c7a14991ca725fda6987b82
KArtem <k6art5@gmail.com>
2024-10-17 16:31:59 +0700

    import getCorrectBalanceWithFormatNumber

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,3 +1,3 @@
-export { formatNumber, formatIntTonNumber, getCorrectBalance } from './formatNumber'
+export { formatNumber, formatIntTonNumber, getCorrectBalance, getCorrectBalanceWithFormatNumber } from './formatNumber'
 export { differenceInSeconds } from './getDifferenceInSeconds'
 export { getStorage, setStorage, removeStorage } from './localStorage'
