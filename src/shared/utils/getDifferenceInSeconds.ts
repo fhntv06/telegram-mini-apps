5fe150b07efcfdace79e136f416fcab0ba053568
kartem <k6art5@gmail.com>
2024-10-20 12:01:22 +0700

    refactor func getDifferenceInSeccond

--- src/shared/utils/getDifferenceInSeconds.ts
+++ src/shared/utils/getDifferenceInSeconds.ts
@@ -1,5 +1,9 @@
 export const differenceInSeconds = (timestamp: number) => {
   const diffTime = -Math.floor((Date.now() - timestamp) / 1000)
 
-  return diffTime === 0 ? '00:00' : diffTime < 10 ? `00:0${diffTime}` : `00:${diffTime}`
+  return (diffTime === 0 || diffTime < 0)
+    ? '00:00'
+    : diffTime < 10
+      ? `00:0${diffTime}`
+      : `00:${diffTime}`
 }
