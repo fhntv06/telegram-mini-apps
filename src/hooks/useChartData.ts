a37cb9f3f77d05816dea7d196f9f47d3ac27e934
kartem <kartemake@gmail.com>
2024-11-19 09:30:44 +0700

    refactor, restore useChartData for work

--- src/hooks/useChartData.ts
+++ src/hooks/useChartData.ts
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react'
 import { ChartData } from 'chart.js'
 import { useSelector } from 'react-redux'
-import { numberLastPoint } from '../shared/constants'
+import { countPointsChart, numberLastPoint } from '../shared/constants.ts'
 
-const createInitialData = (history: any) => ({
-  labels: [...history, '', '', ''].map(() => ''), // added label for create offset x-axis
+const createInitialData = (history: number[]) => ({
+  labels: [...history, '', ''].map(() => ''), // added label for create offset x-axis
   datasets: [
-    { data: history.map((el: any) => el) },
+    { data: history.map((el: number) => el) },
   ],
 })
 
@@ -21,21 +21,9 @@ export const useChartData = () => {
 
   const updateData = () => {
     setChartData((prevData: any) => {
-      const data = prevData.datasets[0].data
-      const labels = prevData.labels;
+      const data = prevData.datasets[0].data;
 
-      // нужно добавить логику чтобы когда цена сильно уходит наверх и startBtcPrice пропадает из виду
-      // то начинать добавить (push) без shift чтобы график растягивался и было видно точку startBtcPrice
-      // и вниз тоже самое
-
-      const biggerTicks = startBtcPrice < Math.min(...data) || startBtcPrice > Math.max(...data)
-
-      if (biggerTicks) {
-        labels.push('')
-      } else {
-        data.shift()
-      }
-
+      data.shift()
       data.push(btcPrice)
 
       return {
@@ -44,11 +32,7 @@ export const useChartData = () => {
           {
             data,
             pointRadius: function (context) {
-              if (biggerTicks) {
-                return context.dataIndex === data.length - 2
-              } else {
-                return context.dataIndex === numberLastPoint  ? 5 : 0;
-              }
+              return context.dataIndex === numberLastPoint ? 5 : 0;
             },
           }
         ],
