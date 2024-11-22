bf5ba015c9336f9299e987994e4ab3f9350c5080
KArtem <k6art5@gmail.com>
2024-11-22 20:59:29 +0700

    refactor hook price history

--- src/hooks/usePriceHistory.ts
+++ src/hooks/usePriceHistory.ts
@@ -1,6 +1,6 @@
 import { getPriceHistory } from '../app/api'
 import { useEffect, useState } from 'react'
-import { countPointsChart, initialDataPriceHistory } from '../shared/constants.ts'
+import { initialDataPriceHistory } from '../shared/constants.ts'
 
 export const usePriceHistory = () => {
   const [priceHistory, setPriceHistory] = useState<number[]>([])
@@ -8,7 +8,7 @@ export const usePriceHistory = () => {
   useEffect(() => {
     console.log('Запрос getPriceHistory!')
     getPriceHistory()
-      .then(async (res) => setPriceHistory(res.data.slice(0,countPointsChart)))
+      .then(async (res) => setPriceHistory(res.data))
       .catch((error) => {
         console.log('Error in getPriceHistory: ', error)
         setPriceHistory(initialDataPriceHistory)
