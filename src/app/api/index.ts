979958ea82bce526d153cb3b1be9988d2a0cc6b5
KArtem <k6art5@gmail.com>
2024-11-23 10:02:57 +0700

    added method getDemoBalance

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -1,3 +1,4 @@
 export { getPriceHistory } from './prices'
 export { getBalance } from './user'
+export { getDemoBalance } from './user'
 export { postReferral } from './referral'
--- src/app/api/user/index.ts
+++ src/app/api/user/index.ts
@@ -3,6 +3,9 @@ import { get , post } from '../core'
 // Get balance in nano TON
 export const getBalance = (address: string) => get(`/api/getBalance/${address}`)
 
+// Get balance in nano TON
+export const getDemoBalance = (telegramId: number) => get(`/api/getDemoBalance/${telegramId}`)
+
 // Get wallet bet in nano TON
 export const getWalletBet = (address: string) => get(`/api/getWalletBet/${address}`)
 
