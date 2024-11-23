ac284733617beb8731e5297637cb8244fef5b0fe
kartem <k6art5@gmail.com>
2024-11-23 19:02:37 +0700

    refactor method api user

--- src/app/api/user/index.ts
+++ src/app/api/user/index.ts
@@ -1,4 +1,4 @@
-import { get , post } from '../core'
+import { get, post } from '../core'
 
 // Get balance in nano TON
 export const getBalance = (address: string) => get(`/api/getBalance/${address}`)
@@ -7,7 +7,7 @@ export const getBalance = (address: string) => get(`/api/getBalance/${address}`)
 export const getDemoBalance = (telegramId: number) => get(`/api/getDemoBalance/${telegramId}`)
 
 // Get wallet bet in nano TON
-export const getWalletBet = (address: string) => get(`/api/getWalletBet/${address}`)
+export const getWalletBet = (data = {}) => post('/api/getWalletBet', data)
 
 // This endpoint records a player's bet details, including their Telegram user image, wallet address, bet amount, and bet status.
-export const postDataBetDetailsPlayers = (data = {}) => post(`/api/players/bet`, data)
+export const postDataBetDetailsPlayers = (data = {}) => post('/api/players/bet', data)
