138505c1bcec9d513005048e094248a46e96f6e0
kartem <kartemake@gmail.com>
2024-12-02 18:08:59 +0700

    rename button change mode => button select mode

---
+++ src/feature/ButtonSelectMode/ButtonSelectMode.tsx
@@ -0,0 +1,31 @@
+import { Button } from '../../shared'
+import classNames from 'classnames/bind'
+import styles from './ButtonSelectMode.module.scss'
+
+const cx = classNames.bind(styles)
+
+interface Props {
+	isActive?: boolean,
+	onClick?: (() => void)
+	className?: string
+}
+
+export const ButtonSelectMode = ({
+	isActive = false,
+	onClick,
+	className,
+}: Props) => {
+	const textMode = '$BTC, 30s'
+
+	return (
+		<Button
+			className={cx('button', className)}
+			type='gray'
+			iconLeftName='bitcoin-medium'
+			iconRightName={isActive ? 'arrow-up-medium' : 'arrow-down-medium'}
+			onClick={onClick}
+		>
+			{textMode}
+		</Button>
+	)
+}
---
+++ src/feature/ButtonSelectMode/index.tsx
@@ -0,0 +1,1 @@
+export { ButtonSelectMode } from './ButtonSelectMode'
