c2fcdffd9a4d2ae89c44a75edbeb46a53463ee04
kartem <kartemake@gmail.com>
2024-12-04 19:56:59 +0700

    change params getGameBetsResult from data(object) to query(string)

--- src/app/api/game/index.ts
+++ src/app/api/game/index.ts
@@ -1,8 +1,7 @@
-import { get, post } from '../core';
-import { string } from 'prop-types'
+import { get } from '../core'
 
 // Returns the current address of the pool smart contract.
 export const getAddressContract = () => get('/api/pool/address')
 
 // Returns the game results including the winning and losing pool data.
-export const getGameBetsResult = (data = { ticker: string, gameMode: string }) => post('/api/game/getGameBetsResult', data)
+export const getGameBetsResult = (query: string) => get(`/api/game/getGameBetsResult?${query}`)
