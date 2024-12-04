2fd9ecf7762fbf7c3b1363c6ff0b4346fb73923e
kartem <kartemake@gmail.com>
2024-12-04 19:55:59 +0700

    refactor hook useSetBalance

--- src/hooks/useSetBalance.ts
+++ src/hooks/useSetBalance.ts
@@ -1,42 +1,41 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { useTonAddress } from '@tonconnect/ui-react'
-import { getBalance, getDemoBalance } from '../app/api/user/'
+import { getBalance, getDemoBalance } from '../app/api'
 import { setUserDataWallet } from '../app/store/slices/user'
-import { useUserData } from './'
-import { isDemoMode } from '../shared/constants'
+import { isDemoMode } from '../shared'
+import WebApp from "@twa-dev/sdk";
 
 export const useSetBalance = () => {
-	const userData = useUserData()
 	const dispatch = useDispatch()
-    const userDataWallet = useSelector((state: any) => state.userDataWallet)
-
-    if (!userData) return 0
-
-	const { gameMode } = useSelector((state: any) => state.modeSettings)
-	const address = useTonAddress()
+  const userDataWallet = useSelector((state: any) => state.userDataWallet)
+  const { gameMode } = useSelector((state: any) => state.modeSettings)
+  const address = useTonAddress()
 
     // TODO: Это убрать в кнопку подключения и перенести в отдельный хук
     const method = gameMode === isDemoMode ? getDemoBalance : getBalance
-    const param = gameMode === isDemoMode ? userData.id : address
+    const param = gameMode === isDemoMode ? WebApp.initData : address
 
     const updateBalance = async () => {
-        console.log('updateBalance')
-        // @ts-ignore
-        const balance = await method(param)
-            .then(res => res.data.balance)
-            .catch((error) => {
-                new Error(error)
+      console.log('updateBalance')
 
-                return 0
-            })
+      // @ts-ignore
+      const balance = await method('param')
+        .then(res => res.data.balance)
+        .catch((error) => {
+          if (!param) new Error(`Error in ${method}: param ${param} is undefined !`)
 
-        dispatch(
-            setUserDataWallet({
-                ...userDataWallet,
-                balance
-            })
-        )
+          new Error(error)
+
+          return 0
+        })
+
+      dispatch(
+        setUserDataWallet({
+          ...userDataWallet,
+          balance
+        })
+      )
     }
 
-    return { updateBalance }
+  return { updateBalance }
 }
