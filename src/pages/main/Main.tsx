5c6f25f6ad148216ee1bb1b612be8c263abb18ad
KArtem <k6art5@gmail.com>
2024-12-17 23:41:01 +0700

    comeback

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-import { useTonAddress } from '@tonconnect/ui-react'
+import { useDispatch } from 'react-redux'
+import { useTonAddress, useTonWallet } from '@tonconnect/ui-react'
 import WebApp from '@twa-dev/sdk'
 import classNames from 'classnames/bind'
 import { AnimatePresence, motion, useWillChange } from 'framer-motion'
@@ -23,9 +23,9 @@ export const Main = () => {
   const data = useGameSocket()
   const priceHistory = usePriceHistory()
   const address = useTonAddress()
-  const { isConnected } = useSelector((state: any) => state.userDataWallet)
   const [skipOnBoarding, setSkipOnBoarding] = useState<boolean>(false)
   const willChange = useWillChange()
+  const wallet = useTonWallet()
 
   // TODO: переписать реализацию получения данных из контекста
   // согласно видео: https://www.youtube.com/watch?v=k2g_Og3CFKU
@@ -52,10 +52,10 @@ export const Main = () => {
 
   // for disconnect action
   useEffect(() => {
-    if (WebApp.initData) {
+    if (WebApp.initData && wallet) {
       handlerPostReferral()
     }
-  }, [isConnected])
+  }, [wallet])
 
   // update data backend
   useEffect(() => {
@@ -75,52 +75,52 @@ export const Main = () => {
         new Error('Error in getAddressContract: ' + error)
 
         dispatch(setDataTransaction({
-            address: import.meta.env.VITE_ADDRESS_TRANSACTION,
-            mainnet: true
-          }))
+          address: import.meta.env.VITE_ADDRESS_TRANSACTION,
+          mainnet: true
+        }))
       })
   }, [])
 
   return (
     isLoading
       ? <LoaderSpinner />
-        : !skipOnBoarding
-          ? (
-            <AnimatePresence>
-              <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{
-                  delay: .1,
-                  duration: .3,
-                  ease: 'easeIn',
-                }}
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  willChange,
-                  height: '100%',
-                }}
-              >
-                <Onboarding handlerSkip={setSkipOnBoarding}/>
-              </motion.div>
-            </AnimatePresence>
-          ) : (
-            <ModalProvider>
-              <NotificationProvider>
-                <AnimationProvider>
-                  <main className={cx('main')}>
-                    <MainHeader/>
-                    <Chart/>
-                    <MainFooter/>
-                  </main>
-                  {!hiddenModalSelectMode && (
-                    <ModalSelectGameMode isOpen={!hiddenModalSelectMode} closeHandler={setHiddenModalSelectMode}/>
-                  )}
-                </AnimationProvider>
-              </NotificationProvider>
-            </ModalProvider>
-          )
+      : !skipOnBoarding
+        ? (
+          <AnimatePresence>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{
+                delay: .1,
+                duration: .3,
+                ease: 'easeIn',
+              }}
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                willChange,
+                height: '100%',
+              }}
+            >
+              <Onboarding handlerSkip={setSkipOnBoarding}/>
+            </motion.div>
+          </AnimatePresence>
+        ) : (
+          <ModalProvider>
+            <NotificationProvider>
+              <AnimationProvider>
+                <main className={cx('main')}>
+                  <MainHeader/>
+                  <Chart/>
+                  <MainFooter/>
+                </main>
+                {!hiddenModalSelectMode && (
+                  <ModalSelectGameMode isOpen={!hiddenModalSelectMode} closeHandler={setHiddenModalSelectMode}/>
+                )}
+              </AnimationProvider>
+            </NotificationProvider>
+          </ModalProvider>
+        )
   )
 }
