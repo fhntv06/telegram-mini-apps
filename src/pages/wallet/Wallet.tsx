7ed11970ccc1967cd29190af50f9208119b0d993
kartem <k6art5@gmail.com>
2024-12-22 02:29:17 +0700

    added button connect in wallet page

--- src/pages/wallet/Wallet.tsx
+++ src/pages/wallet/Wallet.tsx
@@ -1,19 +1,22 @@
+import { useState } from 'react'
+import { useTonAddress } from '@tonconnect/ui-react'
+import { motion } from 'framer-motion'
 import { useSelector } from 'react-redux'
 import classNames from 'classnames/bind'
 
-import styles from './Wallet.module.scss'
+import { useDisconnect, useGetPhrases } from '../../hooks'
+import { ButtonConnectWallet } from '../../feature'
 import {
   Button,
   Icon,
   Select, sourceAltinbit, sourceBitobmen, sourceCryptoBot,
   sourceKotleta, sourceOnemoment, sourcePaybis, sourceWallet
 } from '../../shared'
+
+import styles from './Wallet.module.scss'
+
 import sendIcon from '../../shared/assets/send.png'
 import walletIcon from '../../shared/assets/wallet.png'
-import { useDisconnect, useGetPhrases } from '../../hooks'
-import { useTonAddress } from "@tonconnect/ui-react";
-import { motion } from "framer-motion";
-import { useState } from "react";
 
 const cx = classNames.bind(styles)
 
@@ -124,31 +127,37 @@ export const Wallet = () => {
     <div className={cx('page', 'page-wallet')}>
       <header className={cx('page-wallet__header')}>
         <h2 className='h2-big'>{balance}</h2>
-        <div className={cx('page-wallet__balance')}>
-          <Icon name='ton' size='big' />
-          <h1>{balanseUser}</h1>
+        <div className={cx('page-wallet__balance', { 'not-balance': address })}>
+          {address && <Icon name='ton' size='big' />}
+          <h1>{address ? balanseUser : '- -'}</h1>
         </div>
       </header>
       <main className={cx('page__main')}>
         <div className={cx('container', 'container__buttons')}>
-          <Button
-            className={cx('button__address')}
-            iconLeftName='wallet'
-            iconRightName='copy'
-            sizeIcons='big'
-            type='light'
-            onClick={handlerCopyAddress}
-          >
-            <motion.h2
-              className={cx('copy_text')}
-              initial={{opacity: 0}}
-              animate={isCopied ? {opacity: 1} : {opacity: 0}}
-            >
-              {copied}
-            </motion.h2>
-            <p>{`${address.slice(0, 4)}...${address.slice(address.length - 4)}`}</p>
-          </Button>
-          <Button onClick={handlerDisconnect} iconLeftName='disconnect' sizeIcons='big' />
+          { address ? (
+            <>
+              <Button
+                className={cx('button__address')}
+                iconLeftName='wallet'
+                iconRightName='copy'
+                sizeIcons='big'
+                type='light'
+                onClick={handlerCopyAddress}
+              >
+                <motion.h2
+                  className={cx('copy_text')}
+                  initial={{opacity: 0}}
+                  animate={isCopied ? {opacity: 1} : {opacity: 0}}
+                >
+                  {copied}
+                </motion.h2>
+                <p>{`${address.slice(0, 4)}...${address.slice(address.length - 4)}`}</p>
+              </Button>
+              <Button onClick={handlerDisconnect} iconLeftName='disconnect' sizeIcons='big' />
+            </>
+          ) : (
+            <ButtonConnectWallet className={cx({ 'hide': address })} sizeIcons='big' />
+          ) }
         </div>
         <div className={cx('container')}>
           <header>
