843cb13b4722ed9b4d6e100fcba94525bf144b0c
kartem <kartemake@gmail.com>
2024-12-24 17:49:12 +0700

    create methods api: getLeaderboard getTasks getRetrievesData

--- src/app/api/game/index.ts
+++ src/app/api/game/index.ts
@@ -5,3 +5,9 @@ export const getAddressContract = () => get('/api/pool/address')
 
 // Returns the game results including the winning and losing pool data.
 export const getGameBetsResult = (query: string) => get(`/api/game/getGameBetsResult?${query}`)
+
+// This endpoint retrieves the top players based on their reward points.
+export const getLeaderboard = () => get('/api/leaderboard')
+
+// Retrieve all tasks and hints for the user after verifying the signature and hash.
+export const getTasks = (initData: string) => get(`/api/getTasks/${initData}`)
--- src/app/api/user/index.ts
+++ src/app/api/user/index.ts
@@ -11,3 +11,6 @@ export const getWalletBet = (data = {}) => post('/api/getWalletBet', data)
 
 // This endpoint records a player's bet details, including their Telegram user image, wallet address, bet amount, and bet status.
 export const postDataBetDetailsPlayers = (data = {}) => post('/api/players/bet', data)
+
+// This endpoint retrieves data about a player using their Web App initialization data.
+export const getRetrievesData = (initData: string) => get(`/api/players/data/${initData}`)
