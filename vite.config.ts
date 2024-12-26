e270bb35093e2dd15792a2b7f4624871b81f3ed0
kartem <kartemake@gmail.com>
2024-12-26 22:10:27 +0700

    disabled vite server hot-reload

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    /*here*/
+    hmr: { overlay: false }
+  },
   css: {
     preprocessorOptions: {
       scss: {
