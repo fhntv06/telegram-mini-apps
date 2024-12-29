2517f03f96ac3c9925c06061d4d4d924460d05f5
KArtem <k6art5@gmail.com>
2024-12-29 23:18:40 +0700

    correct hook useSetBalance

--- src/hooks/useSetBalance.ts
+++ src/hooks/useSetBalance.ts
@@ -4,23 +4,24 @@ import { getBalance, getDemoBalance } from '../app/api'
 import { setUserDataWallet } from '../app/store/slices'
 import { useDispatch, useSelector } from './'
 import { isDemoMode } from '../shared'
+import {useEffect, useState} from "react";
 
 export const useSetBalance = () => {
 	const dispatch = useDispatch()
   const userDataWallet = useSelector((state) => state.userDataWallet)
   const { gameMode } = useSelector((state) => state.modeSettings)
   const address = useTonAddress()
+  const [balance, setBalance] = useState<number>(Number(userDataWallet.balance))
 
     // TODO: Это убрать в кнопку подключения и перенести в отдельный хук
     const method = gameMode === isDemoMode ? getDemoBalance : getBalance
     const param = gameMode === isDemoMode ? WebApp.initData : address
 
-    const updateBalance = async () => {
+    const updateBalance = () => {
       console.log('updateBalance')
 
-      // @ts-ignore
-      const balance = await method('param')
-        .then(res => res.data.balance)
+      method(param)
+        .then(res => setBalance(res.data.balance))
         .catch((error) => {
           if (!param) new Error(`Error in ${method}: param ${param} is undefined !`)
 
@@ -28,14 +29,16 @@ export const useSetBalance = () => {
 
           return 0
         })
-
-      dispatch(
-        setUserDataWallet({
-          ...userDataWallet,
-          balance
-        })
-      )
     }
 
-  return { updateBalance }
+  useEffect(() => {
+    dispatch(
+      setUserDataWallet({
+        ...userDataWallet,
+        balance
+      })
+    )
+  }, [balance])
+
+  return { balance, updateBalance }
 }
