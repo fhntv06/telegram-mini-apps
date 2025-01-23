755f5895381674ee69e1add4f0a0798f5bc15b52
KArtem <k6art5@gmail.com>
2025-01-23 22:46:59 +0700

    wip

--- src/app/api/stars/index.ts
+++ src/app/api/stars/index.ts
@@ -1,5 +1,5 @@
 import { get } from '../core'
 
 // Generates and returns a payment invoice link for the specified amount. If the amount is not provided, defaults to 1.
-export const createInvoice = (amount = 1) => get(`/api/createInvoice?amount=${amount}`)
+export const createInvoice = (amount: number = 1) => get(`/api/createInvoice?amount=${amount}`)
 export const getStarsBalance = (initData: string) => get(`/api/getUserBalance/${initData}`)
